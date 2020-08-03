01ebccaf833780e7a23f0703dde456da16400c22
ganha <milo.somera@gmail.com>
2020-08-03 08:59:14 +0800

    Updates

---
+++ precache-manifest.dffd0dab51f46a221702be55ba75402b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6702643a392fd064c63b76483d0805",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "6ee5942fd4d89afcb2ba",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c16cbca92964331a507",
+    "url": "/react-app/static/js/2.41c1b2af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.41c1b2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee5942fd4d89afcb2ba",
+    "url": "/react-app/static/js/main.46b7cc62.chunk.js"
+  },
+  {
+    "revision": "8be9052d1ad5c89b5616",
+    "url": "/react-app/static/js/runtime-main.f234c6d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
